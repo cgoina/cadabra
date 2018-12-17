30717474607df8e1a9a0c54c62db324151b44626
Cristian Goina <goinac@hhmi.org>
2018-12-17 11:42:30 -0500

    moved common code in one place

--- src/QTRAK/qtrak_preprocess/build_ffgrad.sh
+++
@@ -1,1 +0,0 @@
-mex -I/Users/goinac/Work/misc/avbin/include -I/Users/goinac/Work/misc/avbin/libav QTRAK/qtrak_preprocess/mmread/FFGrab.cpp -outdir QTRAK/qtrak_preprocess/mmread -L/Users/goinac/Work/misc/avbin/dist/macosx-x86-64 -lavbin.11 -v
