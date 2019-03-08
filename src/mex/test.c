4a0aae4fdaa44f6796666c48198d6268b2034d3e
Cristian Goina <goinac@janelia.hhmi.org>
2019-03-08 10:14:32 -0500

    working tutorial and FFGrab.exe

---
+++ src/mex/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Entry point.
+ *
+ * @param   argc    command line arguments counter.
+ * @param   argv    command line arguments.
+ *
+ * @return          execution exit code.
+ */
+int main(int argc, char * argv[])
+{
+  printf("It works\n");
+}
